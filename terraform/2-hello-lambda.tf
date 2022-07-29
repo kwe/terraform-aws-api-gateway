0b9019990e05f7e4f69a611ff00accae820d8ccd
Kevin Evans <kwevans@gmail.com>
2022-07-29 15:58:35 +0100

    initial commit

---
+++ terraform/2-hello-lambda.tf
@@ -0,0 +1,58 @@
+resource "aws_iam_role" "hello_lambda_exec" {
+  name               = "hello-lambda"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "hello_lambda_policy" {
+  role       = aws_iam_role.hello_lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+resource "aws_lambda_function" "hello" {
+  function_name = "hello"
+
+  s3_bucket = aws_s3_bucket.lambda_bucket.id
+  s3_key    = aws_s3_object.lambda_hello.key
+
+  runtime = "nodejs16.x"
+  handler = "function.handler"
+
+  source_code_hash = data.archive_file.lambda_hello.output_base64sha256
+
+  role = aws_iam_role.hello_lambda_exec.arn
+}
+
+resource "aws_cloudwatch_log_group" "hello" {
+  name = "/aws/lambda/${aws_lambda_function.hello.function_name}"
+
+  retention_in_days = 7
+}
+
+data "archive_file" "lambda_hello" {
+  type = "zip"
+
+  source_dir  = "../${path.module}/hello"
+  output_path = "../${path.module}/hello.zip"
+}
+
+resource "aws_s3_object" "lambda_hello" {
+  bucket = aws_s3_bucket.lambda_bucket.id
+
+  key    = "hello.zip"
+  source = data.archive_file.lambda_hello.output_path
+
+  etag = filemd5(data.archive_file.lambda_hello.output_path)
+}
